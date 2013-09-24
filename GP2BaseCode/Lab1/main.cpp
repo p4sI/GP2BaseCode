a8a7ba5a6e43598524d55e79f067ebdde274caaa
p4sI <pascalpflueger@gmx.de>
2013-09-24 21:16:13 +0100

    First Running GameApplication added

--- GP2BaseCode/Lab1/main.cpp
+++ GP2BaseCode/Lab1/main.cpp
@@ -5,6 +5,7 @@
 #include <Windows.h>
 #endif
 
+#include "../GameApplication/GameApplication.h"
 
 //Check to see if we are on windows
 #ifdef WIN32
@@ -17,6 +18,28 @@ int WINAPI WinMain(HINSTANCE hInstance,
 int main(int argc, char **argv)
 #endif
 {
+	//new Game Application
+	CGameApplication *pApp = new CGameApplication();
+	//check if it worked
+	if (!pApp->init())
+	{
+		//if something with the init went wrong, delete ptr and set it to NULL
+		if (pApp)
+		{
+			delete pApp;
+			pApp = NULL;
+			return 1;
+		}
+	}
+	//if everything worked, run the Game Application
+	pApp->run();
+	//delete the ptr and set it to NULL
+	if (pApp)
+	{	
+		delete pApp;
+		pApp = NULL;
+	}
+	
 	return 0;
 }
 
