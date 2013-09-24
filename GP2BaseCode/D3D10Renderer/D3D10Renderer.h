efd252b465411a5202bc86b3839cdf2b2036f00e
p4sI <pascalpflueger@gmx.de>
2013-09-24 22:17:40 +0100

    D3D10 Renderer added

---
+++ GP2BaseCode/D3D10Renderer/D3D10Renderer.h
@@ -0,0 +1,39 @@
+#pragma once
+
+//The header file for the renderer interface
+#include "../Renderer/Renderer.h"
+#include <Windows.h>
+
+//forward declarations of the D3D10 interfaces
+struct ID3D10Device;
+struct IDXGISwapChain;
+struct ID3D10RenderTargetView;
+struct ID3D10DepthStencilView;
+struct ID3D10Texture2D;
+
+//D3D10Renderer implements the Renderer interface
+class D3D10Renderer:public IRenderer
+{
+public:
+	//Contructor
+	D3D10Renderer();
+	//Deconstructor
+	~D3D10Renderer();
+
+	//notice these have the same signature as the pure methods
+	bool init(void *pWindowHandle, bool fullScreen);
+	void clear(float r, float g, float b, float a);
+	void present();
+
+private:
+	bool createDevice(HWND pWindowHandle, int windowWidth, int windowHeight,
+			bool fullscreen);
+	bool createInitialRenderTarget(int windowWidth, int windowHeight);
+private:
+	//D3D10 stuff
+	ID3D10Device *m_pD3D10Device;
+	IDXGISwapChain *m_pSwapChain;
+	ID3D10RenderTargetView *m_pRenderTargetView;
+	ID3D10DepthStencilView *m_pDepthStencilView;  //stencil or stencEl ?!?!
+	ID3D10Texture2D *m_pDepthStencilTexture;
+};
