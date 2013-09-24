3850bb723606afd91052a18d84b3090813280c78
p4sI <pascalpflueger@gmx.de>
2013-09-24 20:59:39 +0100

    Added methods: init, run, update, render

--- GP2BaseCode/GameApplication/GameApplication.cpp
+++ GP2BaseCode/GameApplication/GameApplication.cpp
@@ -1,9 +1,9 @@
 #include "GameApplication.h"
 
-// should really check tosee if we are on a windows platform
+// should really check to see if we are on a windows platform
 #include "../Window/Win32Window.h";
 
-//Constructor
+//Constructor (called when a new instance of the GameApplication class is initialised)
 CGameApplication::CGameApplication(VOID)
 {
 	//Set to NULL
@@ -19,7 +19,7 @@ CGameApplication::CGameApplication(VOID)
 	m_ConfigFileName=TEXT("game.cfg");
 }
 
-//Deconstructor
+//Deconstructor (called when deleted)
 CGameApplication::~CGameApplication(void)
 {
 	//Delete things in reverse order
@@ -30,3 +30,90 @@ CGameApplication::~CGameApplication(void)
 	}
 }
 
+//Init
+//This initialises all subsystems
+//If one initialisation fails (return is false), false will be returned
+//otherwise true will be returned
+bool CGameApplication::init()
+{
+	if(!parseConfigFile())
+		return false;
+	if(!initWindow())
+		return false;
+	if(!initGraphics())
+		return false;
+	if(!initInput())
+		return false;
+	if(!initGame())
+		return false;
+	return true;
+}
+
+//called to parse the config file
+bool CGameApplication::parseConfigFile()
+{
+	return true;
+}
+
+//initInput - Initialises the input
+bool CGameApplication::initInput()
+{
+	return true;
+}
+
+//initPhysics - Initialises the physics system
+bool CGameApplication::initPhysics()
+{
+	return true;
+}
+	
+//initGraphics - Initialises the graphic subsystem
+bool CGameApplication::initGraphics()
+{
+	return true;
+}
+
+
+//initWindow - Initialises the window
+bool CGameApplication::initWindow()
+{
+	//Create a Win32 Window
+	m_pWindow = new CWin32Window();
+	m_pWindow->init(m_GameOptionDesc.gameName,m_GameOptionDesc.width,
+					m_GameOptionDesc.height,m_GameOptionDesc.fullscreen);
+
+	return true;
+}
+
+//called to init the game elements
+bool CGameApplication::initGame()
+{
+	return true;
+}
+
+//called to put the game in a loop (aka game loop)
+void CGameApplication::run()
+{
+	//while the window is not closed
+	while(m_pWindow->running())
+	{
+		//check for all window messages
+		m_pWindow->checkForWindowMessages();
+
+		//update
+		update();
+
+		//render
+		render();
+	}
+}
+
+//Render, called to draw one frame of the game
+void CGameApplication::render()
+{
+}
+
+//Update, called to update the game
+void CGameApplication::update()
+{
+}
