81fbf130d6926bc1a3e6b34274e3ce94766226be
p4sI <pascalpflueger@gmx.de>
2013-09-26 20:03:48 +0100

    Added some fcts to D3D10Renderer.cpp

--- GP2BaseCode/D3D10Renderer/D3D10Renderer.cpp
+++ GP2BaseCode/D3D10Renderer/D3D10Renderer.cpp
@@ -3,17 +3,17 @@
 #include <D3D10.h>
 #include <D3DX10.h>
 
-// Constructor
+// Constructor (sets every variables to NULL)
 D3D10Renderer::D3D10Renderer()
 {
 	m_pD3D10Device = NULL;
-	m_pRendererTargetView = NULL;
+	m_pRenderTargetView = NULL;
 	m_pSwapChain = NULL;
 	m_pDepthStencilTexture = NULL;
 	m_pDepthStencilView = NULL;
 }
 
-//Decontrcutor
+//Decontrcutor (Releases each of the Direct3D10 interfaces)
 D3D10Renderer::~D3D10Renderer()
 {
 	if(m_pD3D10Device)
@@ -46,4 +46,45 @@ bool D3D10Renderer::init(void *pWindowHandle, bool fullScreen)
 		return false;
 
 	return true;
+}
+
+bool D3D10Renderer::createDevice(HWND window, int windowWidth, int windowHeight, bool fullScreen)
+{
+	UINT createDeviceFlags = 0;
+#ifdef _DEBUG
+	createDeviceFlags|=D3D10_CREATE_DEVICE_DEBUG;
+#endif
+
+	DXGI_SWAP_CHAIN_DESC sd;
+	ZeroMemory(&sd, sizeof(sd));
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	if(fullScreen)
+		sd.BufferCount = 2;
+	else
+		sd.BufferCount = 1;
+
+	sd.OutputWindow = window;
+	sd.Windowed = (BOOL)(!fullScreen);
+	sd.SampleDesc.Count = 1;
+	sd.SampleDesc.Quality = 0;
+	sd.BufferDesc.Width = windowWidth;
+	sd.BufferDesc.Height = windowHeight;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferDesc.RefreshRate.Numerator = 60;
+	sd.BufferDesc.RefreshRate.Denominator = 1;
+	
+	if(FAILED(D3D10CreateDeviceAndSwapChain(NULL, 
+				D3D10_DRIVER_TYPE_HARDWARE,
+				NULL,
+				createDeviceFlags,
+				D3D10_SDK_VERSION,
+				&sd,
+				&m_pSwapChain,
+				&m_pD3D10Device)
+			  )
+		)
+		return false;
+	
+	return true;
+
 }
