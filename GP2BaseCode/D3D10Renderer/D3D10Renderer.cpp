cd9bc54a7e48e4c927869efcc8c35eff006baa2c
p4sI <pascalpflueger@gmx.de>
2013-09-30 12:10:26 +0100

    added some functions + comments

--- GP2BaseCode/D3D10Renderer/D3D10Renderer.cpp
+++ GP2BaseCode/D3D10Renderer/D3D10Renderer.cpp
@@ -13,7 +13,7 @@ D3D10Renderer::D3D10Renderer()
 	m_pDepthStencilView = NULL;
 }
 
-//Decontrcutor (Releases each of the Direct3D10 interfaces)
+//Deconstructor (Releases each of the Direct3D10 interfaces)
 D3D10Renderer::~D3D10Renderer()
 {
 	if(m_pD3D10Device)
@@ -31,6 +31,11 @@ D3D10Renderer::~D3D10Renderer()
 		m_pD3D10Device->Release();
 }
 
+/* 
+   This function calculates the actual size of the window to be drawn(this is less than the size of window). 
+   Once this size of the drawing area is calculate, the function calls the createDevice 
+   and createInitialRenderTarget functions which are contained in the D3D10Renderer class 
+*/
 bool D3D10Renderer::init(void *pWindowHandle, bool fullScreen)
 {
 	HWND window = (HWND)pWindowHandle;
@@ -48,6 +53,10 @@ bool D3D10Renderer::init(void *pWindowHandle, bool fullScreen)
 	return true;
 }
 
+/* 
+   This function simply creates the device to interface with the graphics hardware 
+   and also a swapchain which holds a series of drawing buffers 
+*/
 bool D3D10Renderer::createDevice(HWND window, int windowWidth, int windowHeight, bool fullScreen)
 {
 	UINT createDeviceFlags = 0;
@@ -73,18 +82,106 @@ bool D3D10Renderer::createDevice(HWND window, int windowWidth, int windowHeight,
 	sd.BufferDesc.RefreshRate.Numerator = 60;
 	sd.BufferDesc.RefreshRate.Denominator = 1;
 	
-	if(FAILED(D3D10CreateDeviceAndSwapChain(NULL, 
-				D3D10_DRIVER_TYPE_HARDWARE,
-				NULL,
-				createDeviceFlags,
-				D3D10_SDK_VERSION,
-				&sd,
-				&m_pSwapChain,
-				&m_pD3D10Device)
-			  )
-		)
+	if (FAILED(D3D10CreateDeviceAndSwapChain(NULL, 
+					D3D10_DRIVER_TYPE_HARDWARE,
+					NULL,
+					createDeviceFlags,
+					D3D10_SDK_VERSION,
+					&sd,
+					&m_pSwapChain,
+					&m_pD3D10Device)))
 		return false;
 	
 	return true;
 
+}
+
+/* 
+   This function grabs the backbuffer from the swap chain 
+   and then creates a depth stencil texture.
+   This function also creates the DepthStencilView and the RenderTargetView, 
+   binds them both to the pipeline and then finally creates a viewport; 
+   which is used in the pipeline for some of the transformations. 
+   The reason why we need to create views is that D3D10 doesn’t directly render to the buffers 
+   it requires views as intermediary between the device and the actual surface, 
+   this helps to protect the video drivers from a crash and also the integrity of video memory. 
+*/
+bool D3D10Renderer::createInitialRenderTarget(int windowWidth, int windowHeight)
+{
+	ID3D10Texture2D *pBackBuffer;
+
+	if (FAILED(m_pSwapChain->GetBuffer(0, 
+								__uuidof(ID3D10Texture2D), 
+								(void**)&pBackBuffer)))
+		return false;
+
+	D3D10_TEXTURE2D_DESC descDepth;
+	descDepth.Width = windowWidth;
+	descDepth.Height = windowHeight;
+	descDepth.MipLevels = 1;
+	descDepth.ArraySize = 1;
+	descDepth.Format = DXGI_FORMAT_D32_FLOAT;
+	descDepth.SampleDesc.Count = 1;
+	descDepth.SampleDesc.Quality = 0;
+	descDepth.Usage = D3D10_USAGE_DEFAULT;
+	descDepth.BindFlags = D3D10_BIND_DEPTH_STENCIL;
+	descDepth.CPUAccessFlags = 0;
+	descDepth.MiscFlags = 0;
+
+	if (FAILED(m_pD3D10Device->CreateTexture2D(&descDepth, 
+									NULL, 
+									&m_pDepthStencilTexture)))
+		return false;
+
+	D3D10_DEPTH_STENCIL_VIEW_DESC descDSV;
+	descDSV.Format = descDepth.Format;
+	descDSV.ViewDimension = D3D10_DSV_DIMENSION_TEXTURE2D;
+	descDSV.Texture2D.MipSlice = 0;
+
+	if (FAILED(m_pD3D10Device->CreateDepthStencilView(m_pDepthStencilTexture,
+									&descDSV, 
+									&m_pDepthStencilView)))
+		return false;
+
+	if (FAILED(m_pD3D10Device->CreateRenderTargetView(pBackBuffer,
+									NULL,
+									&m_pRenderTargetView)))
+	{
+		pBackBuffer->Release();
+		return false;
+	}
+	pBackBuffer->Release();
+
+	m_pD3D10Device->OMSetRenderTargets(1,
+		&m_pRenderTargetView,
+		m_pDepthStencilView);
+
+	D3D10_VIEWPORT vp;
+	vp.Width = windowWidth;
+	vp.Height = windowHeight;
+	vp.MinDepth = 0.0f;
+	vp.MaxDepth = 1.0f;
+	vp.TopLeftX = 0;
+	vp.TopLeftY = 0;
+	m_pD3D10Device->RSSetViewports(1, &vp);
+	return true;
+}
+
+/*
+  This function simply clears all buffers (via the views)
+*/
+void D3D10Renderer::clear(float r, float g, float b, float a)
+{
+	const float ClearColor[4] = {r, g, b, a};
+	m_pD3D10Device->ClearRenderTargetView(m_pRenderTargetView, ClearColor);
+	m_pD3D10Device->ClearDepthStencilView(m_pDepthStencilView, D3D10_CLEAR_DEPTH, 1.0f, 0);
+}
+
+/*
+  This function presents the swapchain which flip the back and the front buffers 
+  -> an image will appear
+*/
+void D3D10Renderer::present()
+{
+	m_pSwapChain->Present(0, 0);
 }
