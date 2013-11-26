0157a9f9e57ea1e6264dc7538605327a8f465516
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2013-11-26 17:07:35 +0000

    Cube implemented

--- GP2BaseCode/GameApplication/CubeVisualComponent.cpp
+++ GP2BaseCode/GameApplication/CubeVisualComponent.cpp
@@ -4,17 +4,28 @@
 
 bool CubeVisualComponent::create(IRenderer * pRenderer)
 {
-	m_iNoVerts=4;
-	Vertex verts[4];
-	verts[0].position=XMFLOAT3(-0.5f,-0.5f,0.0f);
-	verts[1].position=XMFLOAT3(-0.5f,0.5f,0.0f);
-	verts[2].position=XMFLOAT3(0.5f,-0.5f,0.0f);
-	verts[3].position=XMFLOAT3(0.5f,0.5f,0.0f);
+	m_iNoVerts=8;
+	Vertex verts[8];
+	verts[0].position=XMFLOAT3(-0.5f,-0.5f,0.5f);
+	verts[0].normal=XMFLOAT3(-0.25f,-0.25f,0.5f);
+	verts[1].position=XMFLOAT3(-0.5f,0.5f,0.5f);
+	verts[2].position=XMFLOAT3(0.5f,-0.5f,0.5f);
+	verts[3].position=XMFLOAT3(0.5f,0.5f,0.5f);
+	verts[4].position=XMFLOAT3(-0.5f,-0.5f,-0.5f);
+	verts[5].position=XMFLOAT3(-0.5f,0.5f,-0.5f);
+	verts[6].position=XMFLOAT3(0.5f,-0.5f,-0.5f);
+	verts[7].position=XMFLOAT3(0.5f,0.5f,-0.5f);
 
 
 
-	int indices[]={0,1,2,1,3,2};
-	m_iNoIndices=6;
+	int indices[]={0,1,2,1,3,2,		//front 
+					4,5,6,5,7,6,	//back
+					4,5,0,1,0,5,
+					2,3,7,6,7,2,
+					1,5,7,7,3,1,
+					0,4,2,4,6,2
+					};
+	m_iNoIndices=36;
 
 	D3D10Renderer *pD3D10Renderer=static_cast<D3D10Renderer*>(pRenderer);
 
